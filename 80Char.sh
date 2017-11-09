f93c38c1b4132bca725be1181517811ba507e1a3
bpaw <bpaw.dev@gmail.com>
2017-11-09 14:39:38 -0800

    added more options to getopts

--- 80Char.sh
+++ 80Char.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 file=""
-while getopts ":f:" opt; do
+
+d_flag=0	# whether -d flag was passed
+e_flag=0	# whether -e flag was passed
+f_flag=0	# whether -f flag was passed
+
+while getopts ":d:e:f:" opt; do
   case $opt in
+    d)
+      echo "-d flag was specified with $OPTARG as the target directory." >&2
+      d_flag=1
+      ;;
+    e)
+      echo "-e flag was specified with $OPTARG as the target extension." >&2
+      e_flag=1
+      ;;
     f)
       echo "-f flag was triggered with $OPTARG as the target file." >&2
       file=$OPTARG
+      f_flag=1
       ;;
     \?) 
       echo "Invalid option: -$OPTARG" >&2
@@ -17,5 +31,18 @@ while getopts ":f:" opt; do
       ;;
   esac
 done
-echo "The file is: $file"
-grep '.\{81,\}' $file
+
+if [[ $f_flag -eq 1 && $d_flag -eq 1 ]]; then
+  echo "Cannot specify both -d or -f flags." >&2
+  exit 1
+elif [[ $f_flag -eq 1 && $e_flag -eq 1 ]]; then
+  echo "Cannot specify both -e or -f flags." >&2
+  exit 1
+fi
+
+if [[ $f_flag -eq 1 ]]; then 
+  echo "The file is: $file"
+  grep '.\{81,\}' $file
+else
+  echo "-f flag was not specified, so 80Char is in directory mode"
+fi
